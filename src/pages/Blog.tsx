45e01d42bd0f5b7906f5df6af41f3ae2492cf695
Denis Ulvanskyi <denis.ulvanskyi@cengage.com>
2023-09-03 12:11:15 +0300

    suspense

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,36 +1,44 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, Await, useAsyncValue } from "react-router-dom";
 import { IPost } from "../types";
+import { Suspense } from "react";
 import { Spinner } from "../components/Spinner";
+
+const Posts = () => {
+  const posts = useAsyncValue() as IPost[];
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link to={`/blog/${post.id}`}>{post.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export const Blog: React.FC = () => {
   const posts = useLoaderData() as IPost[];
 
-  if (!posts) {
-    return <Spinner />;
-  }
-
-  if (!posts.length) {
-    return <h1>Nothing to show</h1>;
-  }
-
   return (
     <div>
       <h1>Blog</h1>
-      <ul>
-        {posts.map((post) => {
-          return (
-            <li key={post.id}>
-              <Link to={`/blog/${post.id}`}>{post.title}</Link>
-            </li>
-          );
-        })}
-      </ul>
+      <Suspense fallback={<Spinner />}>
+        <Await resolve={posts} children={() => <Posts />} />
+      </Suspense>
     </div>
   );
 };
 
-export const blogLoader = async () => {
+async function getPosts() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
 
-  return response.json();
+  return posts;
+}
+
+export const blogLoader = async () => {
+  const posts = await getPosts();
+
+  return posts;
 };
